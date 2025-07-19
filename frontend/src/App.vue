c144e5ab735649aa7a84b395351aefd1849a421f
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-19 01:32:29 -0700

    feat: landing page template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,31 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div
+    class="h-screen w-full flex flex-col justify-center items-center bg-center bg-cover text-center text-white bg-gray-800 bg-[url(../assets/background.jpg)]">
+    <!-- logo -->
+    <div class="mb-6">
+      <svg class="w-12 h-12" viewBox="0 0 24 24"><!-- … --></svg>
     </div>
-  </header>
 
-  <RouterView />
+    <!-- title / subtitle -->
+    <div class="border-t-1 border-b-1 py-8">
+      <h1 class="text-6xl font-bold tracking-widest">DIMENSION</h1>
+      <p class="mt-4 max-w-2xl leading-relaxed text-lg">
+        A fully responsive site template designed by HTML5 UP and
+        released for free under the Creative Commons license.
+      </p>
+    </div>
+
+    <!-- nav links -->
+    <nav class="mt-18 flex w-full max-w-md mx-auto">
+      <RouterLink to="/auth" class="px-4 py-2 border border-white/70 hover:bg-white/10 transition flex-1 bg-blue-400">
+        Get Started</RouterLink>
+      <RouterLink to="/about" class="px-4 py-2 border border-white/70 hover:bg-white/10 transition flex-1">About
+      </RouterLink>
+    </nav>
+  </div>
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
