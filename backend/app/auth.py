4118cd466f2774c68ec5a4b0f42d868cbafffd37
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-20 03:52:11 -0700

    fix: updated json error returns

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -4,27 +4,28 @@ from flask_jwt_extended import create_access_token
 
 from models import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login', methods=['POST'])
+
+@auth.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
 
     if not email:
-        return jsonify({"msg": "Missing email"}), 400
+        return jsonify({"err": "Missing email"}), 400
     if not password:
-        return jsonify({"msg": "Missing password"}), 400
-    
+        return jsonify({"err": "Missing password"}), 400
+
     try:
         user = User.objects.get(email__iexact=email)
     except DoesNotExist:
-        return jsonify({"msg": "Bad email"}), 401
+        return jsonify({"err": "Bad email"}), 401
     except MultipleObjectsReturned:
-        return jsonify({"msg": "DB integrity broken (identical emails)"}), 500
+        return jsonify({"err": "DB integrity broken (identical emails)"}), 500
     else:
         if not user.check_password(password):
-            return jsonify({"msg": "Bad password"}), 401
-        
+            return jsonify({"err": "Bad password"}), 401
+
         access_token = create_access_token(identity=email)
         return jsonify(access_token=access_token), 200
