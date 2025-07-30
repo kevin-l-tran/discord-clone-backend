6da809f31140ee6fb94e57373197c8c06f644cc1
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-30 00:41:02 -0700

    feat: added modal for creating channels

--- frontend/src/Chat.vue
+++ frontend/src/Chat.vue
@@ -5,15 +5,19 @@ import { useRoute } from 'vue-router';
 import ChannelsList from './components/Chat/ChannelsList.vue';
 import MembersList from './components/Chat/MembersList.vue';
 import MessageChannel from './components/Chat/MessageChannel.vue';
+import CreateChannelModal from './components/Chat/CreateChannelModal.vue';
 import { BACKEND_URL } from './config';
 
 const route = useRoute();
 
 const channels = ref([]);
-const member_data = ref([])
-const members = ref([])
+const member_data = ref([]);
+const members = ref([]);
+const current_member_data = ref();
 const selectedChannel = ref();
 const groupId = computed(() => String(route.params.group_id || ''));
+
+const showCreateTextChannel = ref(false);
 
 const token = localStorage.getItem('access_token') || '';
 watch(
@@ -21,13 +25,16 @@ watch(
     async (id) => {
         if (!id) return;
         try {
-            const [resCh, resMem] = await Promise.all([
+            const [resCh, resMem, resSelf] = await Promise.all([
                 fetch(`${BACKEND_URL}/group/${encodeURIComponent(id)}/channels`, {
                     headers: { Authorization: `Bearer ${token}` },
                 }),
                 fetch(`${BACKEND_URL}/group/${encodeURIComponent(id)}/members`, {
                     headers: { Authorization: `Bearer ${token}` },
                 }),
+                fetch(`${BACKEND_URL}/group/${encodeURIComponent(id)}/members/self`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                })
             ]);
             if (!resCh.ok || !resMem.ok) {
                 throw new Error(`Fetch failed: ${resCh.status}, ${resMem.status}`);
@@ -39,6 +46,8 @@ watch(
             member_data.value = rawList;
             members.value = rawList.map(item => item.user);
 
+            current_member_data.value = await resSelf.json();
+
             selectedChannel.value = channels.value[0];
         } catch (e) {
             console.error('Error loading group data', e);
@@ -47,16 +56,22 @@ watch(
     },
     { immediate: true }
 );
+
+function groupView() {
+    window.location.href = '/groups/';
+}
 </script>
 
 <template>
     <div class="flex h-screen bg-gray-50" v-if="selectedChannel">
         <!-- Channel List (Left) -->
         <aside class="w-60 bg-white border-r border-gray-200 flex flex-col" aria-label="channel list">
-            <div class="flex items-center h-16 px-6 border-b border-gray-200">
-                <h2 class="text-2xl font-bold text-gray-900">Group Name</h2>
+            <div class="flex items-center align-center h-16 px-2 border-b border-gray-200">
+                <button class="text-2xl font-bold text-gray-500 px-2 cursor-pointer" @click="groupView()"> < </button>
+                <h2 class="text-2xl font-bold text-gray-900 px-4">Group Name</h2>
             </div>
-            <ChannelsList :channels="channels" v-model="selectedChannel"></ChannelsList>
+            <ChannelsList :channels="channels" :isOwner="current_member_data.membership.role === 'Owner'"
+                v-model="selectedChannel" @newTextChannel="showCreateTextChannel = true"></ChannelsList>
         </aside>
         <!-- Chat Section -->
         <main class="flex-1 flex flex-col">
@@ -69,8 +84,14 @@ watch(
             </div>
             <MembersList :members="members"></MembersList>
         </aside>
+
+        <Teleport to="body">
+            <CreateChannelModal v-show="showCreateTextChannel" :groupId="groupId" @close="showCreateTextChannel = false"></CreateChannelModal>
+        </Teleport>
     </div>
+
     <div v-else class="p-4 text-center">
         Loading…
     </div>
+
 </template>
